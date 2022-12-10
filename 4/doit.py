614f18a2dfd3c9e96d34e6a04781f43d8f14aa88
Mike Dulaney <mdulaney@imac-2.lan>
2022-12-10 08:41:16 -0500

    add completed day 4

---
+++ 4/doit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+def make_range_set(tok):
+    s1_start, s1_end = tok.split('-')
+    return set(range(int(s1_start), int(s1_end)+1))
+
+f = sys.argv[1]
+
+count = 0
+with open(f, 'r') as inf:
+    for l in inf.readlines():
+        s1, s2 = l.split(',')
+        r1 = make_range_set(s1)
+        r2 = make_range_set(s2)
+
+        print(r1, r2)
+        if r1 >= r2 or r1 < r2:
+            count += 1
+print(count)
